dce3aa4f5315ef0963857f0b0f875297773bf3f7
Caterina Paun <caterina.paun@gmail.com>
2021-05-03 07:35:22 -0700

    Add: starter files for homework 4

---
+++ hw4/01-characters.js
@@ -0,0 +1,4 @@
+// URL to Game of Thrones API to fetch all characters
+let url = 'https://thronesapi.com/api/v2/Characters';
+
+fetch(url);
---
+++ hw4/02-charts.js
@@ -0,0 +1,28 @@
+let backgroundColors = [
+  'rgba(54, 162, 235, 0.8)',
+  'rgba(255, 206, 86, 0.8)',
+  'rgba(255, 99, 132, 0.8)',
+  'rgba(75, 192, 192, 0.8)',
+  'rgba(153, 102, 255, 0.8)',
+  'rgba(255, 159, 64, 0.8)',
+  'rgba(199, 199, 199, 0.8)',
+  'rgba(83, 102, 255, 0.8)',
+  'rgba(40, 159, 64, 0.8)',
+  'rgba(210, 199, 199, 0.8)',
+];
+
+let borderColors = [
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(255, 99, 132, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+  'rgba(159, 159, 159, 1)',
+  'rgba(83, 102, 255, 1)',
+  'rgba(40, 159, 64, 1)',
+  'rgba(210, 199, 199, 1)',
+];
+
+// URL to Game of Thrones API to fetch all characters
+let url = 'https://thronesapi.com/api/v2/Characters';
